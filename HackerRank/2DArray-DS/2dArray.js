11480f068225c980dee33af1336636839223a2f5
MalihaKabir <malihakabir01@gmail.com>
2020-02-03 00:16:24 +0600

    tried to solve it without built in method

--- HackerRank/2DArray-DS/2dArray.js
+++ HackerRank/2DArray-DS/2dArray.js
@@ -7,10 +7,11 @@ const arrayOfHrGlass = [
 	[ 0, 0, 1, 2, 4, 0 ],
 ];
 
-function maxOfHrGlassSum (arr) {
+function maxOfHrGlassSum0 (arr) {
 	let maxSum = [];
 	for (let i = 0, n = arr.length - 3; i <= n; i++) {
 		for (let j = 0, m = arr.length - 3; j <= m; j++) {
+			// finding the sum
 			let hourGlassSum =
 				arr[i][j] +
 				arr[i][j + 1] +
@@ -19,10 +20,37 @@ function maxOfHrGlassSum (arr) {
 				arr[i + 2][j] +
 				arr[i + 2][j + 1] +
 				arr[i + 2][j + 2];
+			// adding in the array
 			maxSum.push(hourGlassSum);
 		}
 	}
-	console.log(Math.max(...maxSum));
+
+	// finding the max sum from the array
+	return Math.max(...maxSum);
 }
 
-maxOfHrGlassSum(arrayOfHrGlass);
+// or
+function maxOfHrGlassSum1 (arr) {
+	let maxSum = [];
+	for (let i = 0, n = arr.length - 3; i <= n; i++) {
+		for (let j = 0, m = arr.length - 3; j <= m; j++) {
+			// finding the sum
+			let hourGlassSum = 0;
+			hourGlassSum += arr[i][j];
+			hourGlassSum += arr[i][j + 1];
+			hourGlassSum += arr[i][j + 2];
+			hourGlassSum += arr[i + 1][j + 1];
+			hourGlassSum += arr[i + 2][j];
+			hourGlassSum += arr[i + 2][j + 1];
+			hourGlassSum += arr[i + 2][j + 2];
+			// adding in the array
+			maxSum.push(hourGlassSum);
+		}
+	}
+
+	// finding the max sum from the array
+	return Math.max(...maxSum);
+}
+
+console.log('maxOfHrGlassSum0', maxOfHrGlassSum0(arrayOfHrGlass));
+console.log('maxOfHrGlassSum1', maxOfHrGlassSum1(arrayOfHrGlass));
---
+++ HackerRank/2DArray-DS/practice2dArray.js
@@ -0,0 +1,54 @@
+// const arrayOfHrGlass = [
+// 	[ 1, 1, 1, 0, 0, 0 ],
+// 	[ 0, 1, 0, 0, 0, 0 ],
+// 	[ 1, 1, 1, 0, 0, 0 ],
+// 	[ 0, 0, 2, 4, 4, 0 ],
+// 	[ 0, 0, 0, 2, 0, 0 ],
+// 	[ 0, 0, 1, 2, 4, 0 ],
+// ];
+const arrayOfHrGlass = [
+	[ -1, 1, -1, 0, 0, 0 ],
+	[ 0, -1, 0, 0, 0, 0 ],
+	[ -1, -1, -1, 0, 0, 0 ],
+	[ 0, -9, 2, -4, -4, 0 ],
+	[ -7, 0, 0, -2, 0, 0 ],
+	[ 0, 0, -1, -2, -4, 0 ],
+];
+
+function maxOfHrGlassSum (arr) {
+	let maxSum = [];
+	for (let i = 0, n = arr.length - 3; i <= n; i++) {
+		for (let j = 0, m = arr.length - 3; j <= m; j++) {
+			// finding the sum
+			let hourGlassSum = 0;
+			hourGlassSum += arr[i][j];
+			hourGlassSum += arr[i][j + 1];
+			hourGlassSum += arr[i][j + 2];
+			hourGlassSum += arr[i + 1][j + 1];
+			hourGlassSum += arr[i + 2][j];
+			hourGlassSum += arr[i + 2][j + 1];
+			hourGlassSum += arr[i + 2][j + 2];
+			// adding in the array
+			maxSum.push(hourGlassSum);
+		}
+	}
+	return maxSum;
+}
+
+function largestSum (arr) {
+	// initiate sum
+	let sum;
+	for (let i = 0, n = arr.length; i < n; i++) {
+		if (!sum) {
+			sum = arr[i];
+		}
+		if (sum < arr[i]) {
+			sum = arr[i];
+		}
+	}
+
+	console.log(sum);
+}
+
+largestSum(maxOfHrGlassSum(arrayOfHrGlass));
+console.log(maxOfHrGlassSum(arrayOfHrGlass));
